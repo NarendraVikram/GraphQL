add5b1ae3f8f57ddd0a3901e9061cdb1018fedaa
NarendraVikram <118122808+NarendraVikram@users.noreply.github.com>
2022-11-13 18:14:48 -0400

    Added conection to database

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 
+const mongoose = require('mongoose');
+
 const schema = require('./Schema/schema');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://gql-vikram:vikram123@cluster0.rg4rq8g.mongodb.net/test');
+
+mongoose.connection.once('open', () => {
+  console.log("connected to Database");
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
